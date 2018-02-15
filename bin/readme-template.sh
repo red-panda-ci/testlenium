4de75298e2683e78406f01e1fd5fa9506425ce73
Pedro Amador <pedro.amador@gigigo.com>
2018-02-15 12:54:08 +0100

    Docs: Set the right build badge

--- bin/readme-template.sh
+++ bin/readme-template.sh
@@ -5,7 +5,7 @@ cat << EOF
 
 Automated testing with Selenium through Zalenium using an ephemeral Docker platform
 
-[![Build Status](http://jenkins.redpandaci.com/buildStatus/icon?job=red-panda-ci/testlenium/develop)](https://jenkins.redpandaci.com/job/red-panda-ci/job/jenkins-pipeline-library/job/develop/) [![Join the chat at https://gitter.im/red-panda-ci/testlenium](https://badges.gitter.im/red-panda-ci/testlenium.svg)](https://gitter.im/red-panda-ci/testlenium?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+[![Build Status](http://jenkins.redpandaci.com/buildStatus/icon?job=red-panda-ci/testlenium/develop)](https://jenkins.redpandaci.com/job/red-panda-ci/job/testlenium/job/develop/) [![Join the chat at https://gitter.im/red-panda-ci/testlenium](https://badges.gitter.im/red-panda-ci/testlenium.svg)](https://gitter.im/red-panda-ci/testlenium?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
 
 ## Usage
 
